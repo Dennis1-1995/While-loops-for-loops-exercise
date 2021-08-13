96b695484646fdf92d9ff34abea269a7787fd330
Dennis1-1995 <83753035+Dennis1-1995@users.noreply.github.com>
2021-08-13 17:12:06 +0200

    Add files via upload

---
+++ even-odd-reporter.js
@@ -0,0 +1,7 @@
+for (let i = 0; i <= 20; i++) {
+    if (i % 2){
+        console.log(`Number ${i} is odd`);
+    }else{
+        console.log(`Number ${i} is even`);
+    }
+}
---
+++ multiplication-tables.js
@@ -0,0 +1,6 @@
+for (var x = 1; x <=10; x++) {
+	for (var y = 1; y <= 10; y++) {
+      var sum =  y + ' x ' + x + ' = ' + x*y;
+      console.log(sum);
+    }
+}
